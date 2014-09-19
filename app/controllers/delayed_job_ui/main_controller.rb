4a6c5d9a4dae19db8e3bb25bc5f657be2bc9a8b6
seanwooj <seanwoojinkim@gmail.com>
2014-09-19 15:40:06 -0700

    saves the error if one happens

--- app/controllers/delayed_job_ui/main_controller.rb
+++ app/controllers/delayed_job_ui/main_controller.rb
@@ -1,8 +1,19 @@
 module DelayedJobUi
   class MainController < ::ApplicationController
     def invoke
+      begin
       @job = Delayed::Job.find(params[:id])
       @job.invoke_job
+      rescue => e
+        puts "[DelayedJobUi] This job failed"
+        puts e.message
+        ap e.backtrace
+        @job.failed_at = Time.zone.now
+        @job.last_error = e.message
+        @job.save
+      else
+        @job.delete # if successful, destroy the job.
+      end
       redirect_to params[:return_url]
     end
 
